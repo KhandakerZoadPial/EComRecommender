145e4d590c5791f9bc1f162d3d676a252e9286da
KhandakerZoadPial <khpial27@gmail.com>
2024-06-23 20:24:50 +0600

    ajax added for realtime search

--- mainapp/scrapers.py
+++ mainapp/scrapers.py
@@ -1,35 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_amazon(product_name):
-    # Example Amazon search URL
-    url = f"https://www.amazon.com/s?k={product_name.replace(' ', '+')}"
-    
-    print(url)
-    headers = {
+
+headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
         'accept-language': 'en-GB,en;q=0.9',
     }
 
+def scrape_amazon(product_name):
+    url = f"https://www.amazon.com/s?k={product_name.replace(' ', '+')}"
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
-    # Extract product information (this is a simplified example)
     results = [{}]
     max_rating = 0
     for item in soup.select('.s-main-slot .s-result-item'):
         my_dict = {
 
         }
-        
-
         try:
             my_dict["title"] = item.select_one('h2 .a-text-normal').get_text()
             rating = int(item.find('a', 'a-link-normal s-underline-text s-underline-link-text s-link-style').find('span', 'a-size-base s-underline-text').get_text())            
             my_dict["rating"] = rating
             my_dict ["star"] = float(item.find('span', 'a-icon-alt').get_text().split(' ')[0])
+            my_dict["image_link"] = item.find('div', 'puis-card-container s-card-container s-overflow-hidden aok-relative puis-include-content-margin puis puis-v36tmkogwwueb520kjutkllt0dc s-latency-cf-section puis-card-border').find('img', 's-image s-image-optimized-rendering')['src']
         except:
             continue
-        
         
         if rating > max_rating:
             max_rating = rating
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
+# scrapers
 from .scrapers import scrape_amazon
 
 # Create your views here.
 def home(request):
     if request.method == "POST":
-        product_name = "I phone 15"
-        print(scrape_amazon(product_name))
-        return HttpResponse("Hi")
+        product_name = request.POST.get('product_name')
+        print(product_name)
+        results = scrape_amazon(product_name)[0]
+        results_data = [
+            {
+                'name': results["title"],
+                'price': results["rating"],
+                'ratings': results["rating"],
+                'image_url': results["image_link"]
+                # 'image_url': product.image_url
+            }
+        ]
+        print(results_data)
+
+        return JsonResponse({'results': results_data})
     else:
         return render(request, "mainapp/home.html")
