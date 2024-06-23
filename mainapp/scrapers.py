b6f7fc7e2e7a18ef75bf71d1774ed49f8cf5a3cf
KhandakerZoadPial <khpial27@gmail.com>
2024-06-24 01:51:49 +0600

    aliexpress scapper added

--- mainapp/scrapers.py
+++ mainapp/scrapers.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
         'accept-language': 'en-GB,en;q=0.9',
+        'country': 'US',
+    }
+cookies = {
+        "aep_usuc_f": "site=glo&c_tp=USD"
     }
 
 def scrape_amazon(product_name):
@@ -23,6 +27,7 @@ def scrape_amazon(product_name):
             my_dict["rating"] = rating
             my_dict ["star"] = float(item.find('span', 'a-icon-alt').get_text().split(' ')[0])
             my_dict["image_link"] = item.find('div', 'puis-card-container s-card-container s-overflow-hidden aok-relative puis-include-content-margin puis puis-v36tmkogwwueb520kjutkllt0dc s-latency-cf-section puis-card-border').find('img', 's-image s-image-optimized-rendering')['src']
+            my_dict["price"] = item.find('span', 'a-color-base').get_text()
         except:
             continue
         
@@ -31,4 +36,28 @@ def scrape_amazon(product_name):
             results[0] = my_dict
         if len(results) >= 20:
             break
+    return results
+
+
+def scrape_aliexpress(product_name):
+    url = f"https://www.aliexpress.com/w/wholesale-{product_name.replace(' ', '-')}.html?spm=a2g0o.productlist.search.0"
+    response = requests.get(url, headers=headers, cookies=cookies)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # print(soup)
+    results = [{}]
+    max_sold = 0
+    for item in soup.select('.list--galleryWrapper--29HRJT4'):
+        my_dict = {}
+        my_dict["image_link"] = 'https:' +  item.find('img', 'images--item--3XZa6xf')['src']
+        my_dict["title"] = item.find('h3', 'multi--titleText--nXeOvyr').get_text()
+        temp = item.find('div', 'multi--price-sale--U-S0jtj').find_all('span')
+        my_dict["price"] = temp[0].get_text() + temp[1].get_text() + temp[2].get_text() + temp[3].get_text() 
+        if int(item.find('span', 'multi--trade--Ktbl2jB').get_text().split(' ')[0]):
+            my_dict["number_of_sold"] = int(item.find('span', 'multi--trade--Ktbl2jB').get_text().split(' ')[0])
+        else:
+            continue
+        print(my_dict)
+        if max_sold < my_dict["number_of_sold"]:
+            results[0] = my_dict
+    
     return results
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.template.loader import render_to_string
 
 # scrapers
-from .scrapers import scrape_amazon
+from .scrapers import scrape_amazon, scrape_aliexpress
 
 # Create your views here.
 def home(request):
@@ -11,6 +11,7 @@ def home(request):
         product_name = request.POST.get('product_name')
         print(product_name)
         results = scrape_amazon(product_name)[0]
+        ali_express_data = scrape_aliexpress(product_name)[0]
         results_data = [
             {
                 'name': results["title"],
@@ -20,8 +21,16 @@ def home(request):
                 # 'image_url': product.image_url
             }
         ]
-        print(results_data)
+        
+        ali_express_data = [
+            {
+                'name': ali_express_data["title"],
+                'price': ali_express_data["price"],
+                'sold': ali_express_data["number_of_sold"],
+                'image_url': ali_express_data["image_link"]
+            }
+        ]
 
-        return JsonResponse({'results': results_data})
+        return JsonResponse({'results': results_data, 'ali_express_data': ali_express_data})
     else:
         return render(request, "mainapp/home.html")
